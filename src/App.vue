0c35e02f72079f7f413e570544ff68fb8268df62
Pedro Mário Damasceno Maciel Pereira Correia <51101722+PedroCorr@users.noreply.github.com>
2021-11-03 23:34:18 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -15,17 +15,4 @@ html, body, #pp {
   margin: 0;
   box-sizing: border-box;
 }
-/* #nav {
-  padding: 30px;
-  background-color: rgb(110, 105, 105);
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
 </style>
